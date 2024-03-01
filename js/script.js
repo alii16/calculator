aaf0b6d370c6db98ff7636f21a0df52b62a70ed9
Ali Polanunu <154661557+alii16@users.noreply.github.com>
2024-03-01 18:14:10 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let displayValue = '';
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function backSpace() {
+    displayValue = displayValue.slice(0, -1);
+    document.getElementById('display').value = displayValue;
+}
+
+function calculate() {
+    try {
+        const result = eval(displayValue);
+        displayValue = result.toString();
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        displayValue = 'Error';
+        document.getElementById('display').value = displayValue;
+    }
+}
+
